5460a065e0479bf0a49b785422c86279b4d025c1
jasim0021 <skjasimuddin9064@gmail.com>
2024-08-16 13:32:46 +0530

    Initial Commit

---
+++ AppFremwork/AppFremworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppFremworkApp.swift
+//  AppFremwork
+//
+//  Created by Sk Jasimuddin on 16/08/24.
+//
+
+import SwiftUI
+
+@main
+struct AppFremworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppFremwork/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AppFremwork
+//
+//  Created by Sk Jasimuddin on 16/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
